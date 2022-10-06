a49ae91eb19c639faddec4f7a2485507130b334c
aaaluzzi <aaaluzzi@cpp.edu>
2022-10-05 19:35:57 -0700

    Add initial pseudocode

---
+++ tictactoe.c
@@ -0,0 +1,48 @@
+//tic tac toe - either between player or computer
+
+//store board as array of chars
+//boolean for if playing against computer 
+
+/*main method
+    welcome user and ask whether they want to play against a player or another computer
+    validate input
+    if computer, set boolean to true
+
+    loop forever
+        player one turn
+        if won(player 1 piece)
+            player one wins
+            break
+        if tie
+            tie
+            break
+        player two turn
+        if won(player 2 piece)
+            player two wins
+            break
+*/
+
+/*player one turn
+    prompt user to enter number 
+    validate input
+    place x on board
+*/
+
+/*player two turn
+    if cpu, generate random input for player 2
+    else prompt user to enter number
+        validate input
+    place o on board
+*/
+
+/*check board(piece)
+    loop through array and check rows and columns and diagonal
+    return boolean for win
+*/
+
+/*output board
+    loop through array and print grid with board values
+*/
+
+
+
